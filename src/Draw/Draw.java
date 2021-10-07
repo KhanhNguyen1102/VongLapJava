06e5a633b83e7880f3f6068fa7a96cf861c1ff8f
KhanhNguyen1102 <sukhanh12@gmail.com>
2021-10-07 21:52:07 +0700

    draw

---
+++ src/Draw/Draw.java
@@ -0,0 +1,55 @@
+package Draw;
+
+import java.util.Scanner;
+
+public class Draw {
+    public static void main(String[] args) {
+        int choice = -1;
+        Scanner input = new Scanner(System.in);
+        while(choice != 0) {
+            System.out.println("Menu");
+            System.out.println("1. Print the rectangle");
+            System.out.println("2. Print the square triangle");
+            System.out.println("3. Print isosceles triangle");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Print the rectangle");
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 9; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Print the square triangle");
+                    for (int i = 0; i < 7; i++) {
+                        for (int j = 0; j <= i ; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Print isosceles triangle");
+                    for(int i = 1; i <= 6; i++) {
+                        for(int j = 1; j <= 6-i; j++) {
+                            System.out.print("  ");
+                        }
+                        for(int k = 1; k <= 2*i-1; k++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println("");
+                    }
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice!");
+            }
+        }
+    }
+}
